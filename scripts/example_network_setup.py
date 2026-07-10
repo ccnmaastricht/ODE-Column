a74aa5ffa2b7df4c05d32454cf50c8e022b2a628
dasjadeleeuw <dasjadeleeuw@hotmail.com>
2026-07-10 10:15:15 +0200

    example script update

--- scripts/example_network_setup.py
+++ scripts/example_network_setup.py
@@ -1,12 +1,11 @@
 from src.brain_network import BrainNetwork
 
 
-
 if __name__ == '__main__':
 
     # Building the network
     config_path = '../config/example_params.toml'
-    network = BrainNetwork(config_path)
+    network = BrainNetwork.from_toml(config_path)
 
     # network.add_area(area_name='v1', unique_id='v1a', size=2)
     # network.add_area(area_name='v1', unique_id='v1b', size=1)
@@ -28,15 +27,41 @@ if __name__ == '__main__':
     network.add_input_connection(target_area='v1', input_size=2, trainable=True)
     network.add_output_connection(source_area='v2', trainable=True)
 
+    # network.add_area(area_name='v1', size=4, intrinsic_trainable=True, background_trainable=True)
+    # network.add_area(area_name='v2', size=4)
+    #
+    # network.add_feedforward_connection(source='v1', target='v2', trainable=True,
+    #                              receptive_field_size=1,
+    #                              stride=1,
+    #                                    grid_organization=True)
+    # network.add_feedback_connection(source='v2', target='v1', trainable=True)
+    # network.add_lateral_connection(area_name='v1',
+    #                                trainable=True,
+    #                              receptive_field_size=2,
+    #                              stride=2)
+    #
+    # network.add_input_connection(target_area='v1',
+    #                              input_size=9,
+    #                              trainable=True,
+    #                              receptive_field_size=2,
+    #                              stride=1,
+    #                              grid_organization=True)
+    # network.add_output_connection(source_area='v2', trainable=True)
+
     # Stimulus batch (size = 3)
-    stim = [[20.,  0.],
+    stim = [[ 0., 10.],
             [ 0., 20.],
-            [10., 10.]]
+            [ 0., 30.]]
+
+    # stim = [[20., 0., 0., 0., 0., 0., 0., 0., 0.],
+    #         [0., 0., 0., 20., 0., 0., 0., 20., 0.],
+    #         [0., 0., 10.,0., 0., 0., 0., 0., 0.]]
 
     # Run simulation
-    output = network.run(stim, adjoint=False, stochastic=True, device='cpu')
+    output = network.run(stim, adjoint=False, stochastic=False, device='cpu')
     model_read_out = network.read_out(output, mode='classification')
 
     # Plot all firing rates
     firing_rates = network.get_firing_rates(output)
-    network.analysis.plot_firing_rates(firing_rates)
+    # network.analysis.plot_firing_rates(firing_rates, column=0)
+    network.analysis.plot_firing_rates(firing_rates, population=['L23e', 'L5e'])
