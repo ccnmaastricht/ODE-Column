9433e79579e2cd3da1e4270319164bee7229404b
dasjadeleeuw <dasjadeleeuw@hotmail.com>
2026-07-20 11:30:38 +0200

    functionality for inspecting network connections and their weights

--- src/analysis/network_analyzer.py
+++ src/analysis/network_analyzer.py
@@ -11,55 +11,10 @@ class NetworkAnalyzer:
 
     def plot_firing_rates(self, raw_state, sample=None, area=None, column=None, population=None):
         """
-        Plot firing rates
+        Plot firing rates.
         """
         firing_rates, sample_list, area_list, column_list, pop_list = self.network.readout.get_firing_rates(
             raw_state, sample, area, column, population, return_as_dict=True)
-
-        # sample_list     = np.arange(firing_rates.shape[1])
-        # area_list       = self.network.areas.keys()
-        # column_list     = {area_id: np.arange(area.num_columns) for area_id, area in self.network.areas.items()}
-        # pop_list        = np.arange(8)
-        #
-        # if area is not None:
-        #     area_slices = self.network.area_slices[area]
-        #     area_list = [area]
-        #
-        #     firing_rates = {area: firing_rates[:, :, area_slices]}
-        # else:
-        #     firing_rates = {area_id: firing_rates[:, :, area_slice]
-        #                    for area_id, area_slice in self.network.area_slices.items()}
-        #
-        # firing_rates = {area_id: fr.reshape(fr.shape[0],
-        #                                  fr.shape[1],
-        #                                  fr.shape[2] // 8,
-        #                                  8)
-        #                 for area_id, fr  in firing_rates.items()}
-        #
-        # if sample is not None:
-        #     if isinstance(sample, int):
-        #         sample = [sample]
-        #     firing_rates = {area_id : fr[:, sample, :] for area_id, fr in firing_rates.items()}
-        #     sample_list = sample
-        #
-        # if column is not None:
-        #     if isinstance(column, int):
-        #         column = [column]
-        #
-        #     firing_rates = {area_id : fr[:, :, column, :] for area_id, fr in firing_rates.items()}
-        #     column_list = {area_id : column for area_id in firing_rates.keys()}
-        #
-        # if population is not None:
-        #     if not isinstance(population, list):
-        #         population = [population]
-        #
-        #     for i, pop in enumerate(population):
-        #         if isinstance(pop, str):
-        #             population[i] = self.layer_labels.index(pop)
-        #
-        #     firing_rates = {area : fr[:, :, :, population] for area, fr in firing_rates.items()}
-        #
-        #     pop_list = population
 
         for sample_ in range(len(sample_list)):
 
@@ -71,6 +26,67 @@ class NetworkAnalyzer:
                         label = f'{area_}_col{column_list[area_][col]}_{pop_name}'
                         plt.plot(firing_rates[area_][:, sample_, col, pop], label=label)
 
-            plt.legend() # bbox_to_anchor=(1.05, 1), loc='upper left')
+            plt.legend()
             plt.title(f'Sample {sample_list[sample_]}')
             plt.show()
+
+    def get_weights(self, conn_name=None, get_constrained_weights=True, return_as_np=True):
+        """
+        Returns a dictionary of all connection in the network with their
+        weight matrices as values. If param 'conn_name' is specified, the
+        function returns only the weights of this connection.
+        """
+        if get_constrained_weights:
+            conn_dict = {name : conn.W for name, conn in self.network.connections.items()}
+        else:
+            conn_dict = {name : conn.weights for name, conn in self.network.connections.items()}
+
+        if return_as_np:
+            conn_dict = {name : weights.detach().cpu().numpy() for name, weights in conn_dict.items()}
+
+        if conn_name is not None:
+            return conn_dict[conn_name]
+
+        return conn_dict
+
+    def visualize_weights(self, conn_name=None, get_constrained_weights=False):
+        """
+        Visualize the weights as a heatmap.
+        """
+        conn_dict = self.get_weights(conn_name, get_constrained_weights)
+
+        for name, weights in conn_dict.items():
+            fig, ax = plt.subplots()
+
+            heatmap = ax.imshow(weights, cmap="viridis", interpolation="nearest")
+            fig.colorbar(heatmap, ax=ax)
+
+            ax.set_title(name)
+            ax.set_xlabel("Source")
+            ax.set_ylabel("Target")
+
+            plt.show()
+
+    def summarize_connections(self):
+        """
+        Prints a summary of all initialized network connections.
+        """
+        header = (
+            f"{'Name':35}"
+            f"{'Source':15}"
+            f"{'Target':15}"
+            f"{'Shape':15}"
+            f"{'Trainable':10}"
+            f"{'Type':12}"
+        )
+        print(header)
+        print("-" * len(header))
+
+        for name, conn in self.network.connections.items():
+            print(
+                f"{name:35}"
+                f"{str(conn.source_id):15}"
+                f"{str(conn.target_id):15}"
+                f"{str((conn.target_size, conn.source_size)):15}"
+                f"{str(conn.trainable):10}"
+                f"{str(conn.conn_type):12}")
